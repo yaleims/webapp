b2ad783474d278e7806be3ca9a3d8d7179c90239
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Mac-Pro-2.local>
2014-03-02 17:03:54 -0500

    Changed losses to loss

--- yaleIMs/app/scripts/directives/sportlbrow.js
+++ yaleIMs/app/scripts/directives/sportlbrow.js
@@ -3,14 +3,14 @@
 angular.module('yaleImsApp')
     .directive('sportlbRow', function () {
         return {
-            template: '<td>{{rank}}</td><td>{{college}}</td><td>{{points}}</td><td>{{wins}} - {{losses}}</td>',
+            template: '<td>{{rank}}</td><td>{{college}}</td><td>{{points}}</td><td>{{wins}} - {{loss}}</td>',
             restrict: 'A',
             scope: {
                 rank: '@',
                 college: '@',
                 points: '@',
                 wins: '@',
-                losses: '@'
+                loss: '@'
             }
         };
     });
