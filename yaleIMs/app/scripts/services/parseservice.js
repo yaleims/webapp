b3e710ff9e6f7424bb0eadc1d2953e299b3b9f79
ohsnapitscolin <colin.dunn15@gmail.com>
2014-03-02 15:24:17 -0500

    added parse functionality

--- yaleIMs/app/scripts/services/parseservice.js
+++ yaleIMs/app/scripts/services/parseservice.js
@@ -1,6 +1,37 @@
 'use strict';
 
 angular.module('yaleImsApp')
-  .service('Parseservice', function Parseservice() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+	.factory('ParseService', function() {
+ 
+  	Parse.initialize("OIrcvapOVEU2B8RqVp7uzyLavPl5WExmOR5Bw9ph", "ljWrwJntFsQhK6GxZOIVEWYH2SFvslPbiQoNN4Nv");  
+
+    var ParseService = {
+      name: "Parse",
+
+      getColleges: function GetColleges(callback) {
+		var GameScore = Parse.Object.extend("College");
+		var query = new Parse.Query(GameScore);
+		var colleges = [];
+
+		query.find({
+		  	success: function(results) {
+		  		
+		  	  	//Do something with the returned Parse.Object values
+			  	for (var i = 0; i < results.length; i++) { 
+			    	var object = results[i];
+			    	colleges.push({
+						name : object.get('College'),			
+						score : object.get('Score')
+					});
+			    }   
+			    callback(colleges);
+				},
+		  		error: function(error) {
+		    		alert("Error: " + error.code + " " + error.message);
+		  		}
+			});
+		}	
+     };
+
+     return ParseService;
+});
