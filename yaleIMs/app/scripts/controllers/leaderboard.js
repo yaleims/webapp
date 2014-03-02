9513679c711688851580bbd30d06bfdf14eca9c7
ohsnapitscolin <colin.dunn15@gmail.com>
2014-03-02 15:36:54 -0500

    added parse

--- yaleIMs/app/scripts/controllers/leaderboard.js
+++ yaleIMs/app/scripts/controllers/leaderboard.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('yaleImsApp')
-  .controller('LeaderboardCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('LeaderboardCtrl', function ($scope, ParseService) {
+  
+	ParseService.getColleges(function(results) {
+      $scope.$apply(function() {
+        $scope.colleges = results;
+      })
+    });
+});
