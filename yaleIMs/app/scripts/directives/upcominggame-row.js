c76f98451fd16265cd041624a1a76a5ffe96383e
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Computer-2.local>
2014-03-19 19:11:47 -0400

    Added RSVP button to game row

--- yaleIMs/app/scripts/directives/upcominggame-row.js
+++ yaleIMs/app/scripts/directives/upcominggame-row.js
@@ -3,7 +3,7 @@
 angular.module('yaleImsApp')
   .directive('gameRow', function () {
     return {
-        template: '<td ng-hide="{{noicon}}"><flaticon sport="{{sport}}" sporturl="{{sporturl}}" size="smallSportIcon"></flaticon></td><td>{{date}}</td><td>{{team1}} vs. {{team2}}</td><td ng-hide="{{!past}}">{{score1}} - {{score2}}</td><td ng-hide="{{!rsvp}}">RSVP</td><td ng-hide="{{!tyng}}">{{tyngpoints}}</td>',
+        template: '<td ng-hide="{{noicon}}"><flaticon sport="{{sport}}" sporturl="{{sporturl}}" size="smallSportIcon"></flaticon></td><td>{{date}}</td><td>{{team1}} vs. {{team2}}</td><td ng-hide="{{!past}}">{{score1}} - {{score2}}</td><td ng-hide="{{!rsvp}}"><button type="button" class="btn btn-primary btn-sm">Attend</button></td><td ng-hide="{{!tyng}}">{{tyngpoints}}</td>',
         restrict: 'A',
         scope: {
             sport: '@',
