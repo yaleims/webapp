c1f34d44692e5f1224ec584ba42638abad694dd0
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Computer-2.local>
2014-03-19 17:03:36 -0400

    Changed position of date and sport icon

--- yaleIMs/app/scripts/directives/upcominggame-row.js
+++ yaleIMs/app/scripts/directives/upcominggame-row.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('yaleImsApp')
-  .directive('upcomingGameRow', function () {
+  .directive('gameRow', function () {
     return {
-        template: '<td><flaticon sport="{{sport}}" sporturl="{{sporturl}}" size="smallSportIcon"></flaticon></td><td>{{date}}</td><td>{{team1}} vs. {{team2}}</td>',
+        template: '<td><flaticon sport="{{sport}}" sporturl="{{sporturl}}" size="smallSportIcon"></flaticon></td><td>{{date}}</td><td>{{team1}} vs. {{team2}}</td><td ng-hide="{{!past}}">{{score1}} - {{score2}}</td>',
         restrict: 'A',
         scope: {
             sport: '@',
@@ -11,6 +11,9 @@ angular.module('yaleImsApp')
             date: '@',
             team1: '@',
             team2: '@',
+            score1: '@',
+            score2: '@',
+            past: '@'
         }
     };
   });
