49ff1c7e2f9a9371c9624bda22e4a1d2d03e7b24
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Computer-2.local>
2014-03-19 20:27:30 -0400

    Made some changes to profile

--- yaleIMs/app/scripts/controllers/profile.js
+++ yaleIMs/app/scripts/controllers/profile.js
@@ -2,7 +2,7 @@
 
 angular.module('yaleImsApp')
   .controller('ProfileCtrl', function ($scope) {
-    $scope.playerTeams = [{sport: "Badminton", sporturl: "badminton"}, {sport: "Swimming", sporturl: "swimming"}];
+    $scope.playerTeams = [{sport: "Coed Badminton", sporturl: "coed-badminton"}, {sport: "Coed Swimming", sporturl: "coed-swimming"}];
     $scope.playerName = "Nicholas Gonzalez";
         $scope.playerCollege = "Morse";
         $scope.playerYear = 2015;
