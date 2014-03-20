4dc9273d7b38e16d8c4bb12e1293d1353d93d392
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Computer-2.local>
2014-03-19 20:10:59 -0400

    Setup profile page to be unique to user

--- yaleIMs/app/scripts/controllers/profile.js
+++ yaleIMs/app/scripts/controllers/profile.js
@@ -2,9 +2,8 @@
 
 angular.module('yaleImsApp')
   .controller('ProfileCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.playerTeams = [{sport: "Badminton", sporturl: "badminton"}, {sport: "Swimming", sporturl: "swimming"}];
+    $scope.playerName = "Nicholas Gonzalez";
+        $scope.playerCollege = "Morse";
+        $scope.playerYear = 2015;
   });
