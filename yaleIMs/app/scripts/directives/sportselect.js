d40ae3c8012474e2955e2e423c40f8ec2fe41478
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Mac-Pro-2.local>
2014-03-29 22:54:32 -0400

    Made tile selection full page for college page when no sport selected

--- yaleIMs/app/scripts/directives/sportselect.js
+++ yaleIMs/app/scripts/directives/sportselect.js
@@ -3,7 +3,7 @@
 angular.module('yaleImsApp')
   .directive('sportSelect', function () {
     return {
-      template: '<div class="{{colsize}}"><a title="{{sport}}" class="" ng-href="{{url}}"><center><span class="{{iconsize}} flaticon-{{sporturl}}"></span></center></a></div>',
+      template: '<div class="{{colsize}}"><a title="{{sport}}"  ng-href="{{url}}"><center><span class="{{iconsize}} flaticon-{{sporturl}}"></span></center></a></div>',
       replace: true,
       restrict: 'E',
         scope: {
