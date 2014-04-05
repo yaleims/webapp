4aff0bc892122664e804a0d7a2b1026f74de7f78
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Computer-2.local>
2014-04-05 17:13:49 -0400

    Made it so sport select directive calls parent controller function on college page

--- yaleIMs/app/scripts/controllers/college.js
+++ yaleIMs/app/scripts/controllers/college.js
@@ -10,6 +10,23 @@ angular.module('yaleImsApp')
         $scope.collegeURL = college;
         $scope.sportURL = sport;
 
+        $scope.updateSchedule = function (sportName, sportUrl)
+        {
+            console.log(sportName);
+            $scope.sportURL = sportUrl;
+            $scope.sportName = sportName;
+            sport = sportName;
+
+            ParseService.getSports(function(results) {
+                $scope.$apply(function() {
+                    $scope.sportWins = results[0].win;
+                    $scope.sportLosses = results[0].loss;
+                    $scope.sportTyngPoints = results[0].points;
+                    console.log(results);
+                });
+            }, sport, college);
+        }
+
         ParseService.getAllSports(function(results) {
             $scope.allSports = results;
         });
@@ -19,27 +36,17 @@ angular.module('yaleImsApp')
             $scope.totalTyngPoints = results[0].totalTyngPoints;
             $scope.collegeName = results[0].collegeName;
 
-            ParseService.getSportFromUrl(sport, function(sportName){
-                sport = sportName;
-                $scope.sportName = sportName;
+            ParseService.getGames(function(results) {
+                $scope.$apply(function() {
+                    $scope.pastGames = results;
+                    console.log(results);
+                })
+            }, undefined, college, true);
 
-                ParseService.getSports(function(results) {
-                    $scope.sportWins = results[0].win;
-                    $scope.sportLosses = results[0].loss;
-                    $scope.sportTyngPoints = results[0].points;
-                }, sport, college);
-
-                ParseService.getGames(function(results) {
-                    $scope.$apply(function() {
-                        $scope.pastGames = results;
-                    })
-                }, sport, college, true);
-
-                ParseService.getGames(function(results) {
-                    $scope.$apply(function() {
-                        $scope.upcomingGames = results;
-                    })
-                }, sport, college, false);
-            });
+            ParseService.getGames(function(results) {
+                $scope.$apply(function() {
+                    $scope.upcomingGames = results;
+                })
+            }, undefined, college, false);
         });
 }]);
--- yaleIMs/app/scripts/directives/sportselect.js
+++ yaleIMs/app/scripts/directives/sportselect.js
@@ -3,15 +3,17 @@
 angular.module('yaleImsApp')
   .directive('sportSelect', function () {
     return {
-      template: '<div class="{{colsize}}"><a title="{{sport}}"  ng-href="{{url}}"><center><span class="{{iconsize}} flaticon-{{sporturl}}"></span></center></a></div>',
-      replace: true,
+      template: '<div><div ng-if="updatepage"  ng-click="dial()" class="{{colsize}}"><a title="{{sport}}"  ng-href="{{url}}"><center><span class="{{iconsize}} flaticon-{{sporturl}}"></span></center></a></div><div ng-if="!updatepage" class="{{colsize}}"><a title="{{sport}}"  ng-href="{{url}}"><center><span class="{{iconsize}} flaticon-{{sporturl}}"></span></center></a></div></div>',
       restrict: 'E',
+        replace: 'true',
         scope: {
             sport: '@',
             sporturl: '@',
             iconsize: '@',
             colsize: '@',
-            url: '@'
+            url: '@',
+            dial: "&",
+            updatepage: '@'
         }
       }
   });
