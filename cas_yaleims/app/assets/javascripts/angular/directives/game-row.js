2bec844bdead63ac11c4bbfd266896ee342d5470
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Mac-Pro-2.local>
2014-04-18 03:18:22 -0400

    RSVP button directive

--- cas_yaleims/app/assets/javascripts/angular/directives/game-row.js
+++ cas_yaleims/app/assets/javascripts/angular/directives/game-row.js
@@ -3,7 +3,7 @@
 angular.module('yaleImsApp')
   .directive('gameRow', function () {
     return {
-        template: '<td ng-hide="{{noicon}}"><flaticon sport="{{sport}}" sporturl="{{sporturl}}" size="smallSportIcon"></flaticon></td><td>{{date}}</td><td><shield college="{{team1}}" collegeurl="{{team1url}}" size="smallShield"></shield><a ng-href="/#/college/{{team1url}}">{{team1abbr}}</a> vs. <shield college="{{team2}}" collegeurl="{{team2url}}" size="smallShield"></shield><a ng-href="/#/college/{{team2url}}">{{team2abbr}}</a></td><td ng-hide="{{!past}}">{{score1}} - {{score2}}</td><td ng-hide="{{!rsvp}}"><button type="button" class="btn btn-primary btn-sm">Attend</button></td><td ng-hide="{{!outcome}}">{{outcome}}</td><td ng-hide="{{!tyng}}">{{tyngpoints}}</td>',
+        template: '<td ng-hide="{{noicon}}"><flaticon sport="{{sport}}" sporturl="{{sporturl}}" size="smallSportIcon"></flaticon></td><td>{{date}}</td><td><shield college="{{team1}}" collegeurl="{{team1url}}" size="smallShield"></shield><a ng-href="/#/college/{{team1url}}">{{team1abbr}}</a> vs. <shield college="{{team2}}" collegeurl="{{team2url}}" size="smallShield"></shield><a ng-href="/#/college/{{team2url}}">{{team2abbr}}</a></td><td ng-hide="{{!past}}">{{score1}} - {{score2}}</td><td ng-hide="{{!rsvp}}"><rsvp-button netid="{{netid}}" gameid="{{gameid}}" attending="{{attending}}"></rsvp-button></td><td ng-hide="{{!outcome}}">{{outcome}}</td><td ng-hide="{{!tyng}}">{{tyngpoints}}</td>',
         restrict: 'A',
         scope: {
             sport: '@',
@@ -21,7 +21,10 @@ angular.module('yaleImsApp')
             noicon: '@',
             tyng: '@',
             rsvp:  '@',
-            outcome: '@'
+            outcome: '@',
+            netid: '@',
+            gameid: '@',
+            attending: '@'
         }
     };
   });
