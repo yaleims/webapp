e47529502b8aa04a61d066ce365e1d7c2b09cc53
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Mac-Pro-2.local>
2014-04-19 01:33:35 -0400

    prep for profile modal and changed to use urls to join

--- cas_yaleims/app/assets/javascripts/angular/controllers/profile.js
+++ cas_yaleims/app/assets/javascripts/angular/controllers/profile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('yaleImsApp')
-  .controller('ProfileCtrl', ['$scope', '$rootScope', 'Student', 'TeamsService', function ($scope, $rootScope, Student, TeamsService) {
+  .controller('ProfileCtrl', ['$scope', '$rootScope', '$modal', 'Student', 'TeamsService', function ($scope, $rootScope, $modal, Student, TeamsService) {
     $scope.student = $rootScope.student;
 
     $scope.teams = TeamsService.allTeamsWithJoined($scope.student.id);
@@ -10,8 +10,5 @@ angular.module('yaleImsApp')
     	$scope.showModal = !$scope.showModal;
     };
 
-    // $scope.playerTeams = [{sport: "Coed Badminton", sporturl: "coed-badminton"}, {sport: "Coed Swimming", sporturl: "coed-swimming"}];
-    // $scope.playerName = "Nicholas Gonzalez";
-    //     $scope.playerCollege = "Morse";
-    //     $scope.playerYear = 2015;
+    
   }]);
