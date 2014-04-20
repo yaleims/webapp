e0a37bddf556ef77d776e0af2c047961c37bfda1
ohsnapitscolin <colin.dunn15@gmail.com>
2014-04-20 03:15:52 -0400

    we're going bitches

--- cas_yaleims/app/assets/javascripts/angular/controllers/newsfeed.js
+++ cas_yaleims/app/assets/javascripts/angular/controllers/newsfeed.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('yaleImsApp')
-  .controller('NewsfeedCtrl', ['$scope', '$rootScope', 'ParseService', function ($scope, $rootScope, ParseService) {
+  .controller('NewsfeedCtrl', ['$scope', '$rootScope', 'ParseService', 'GamesService', function ($scope, $rootScope, ParseService, GamesService) {
 
+    var netid = $rootScope.student.id;
+    
     ParseService.getColleges(undefined, function(results) {
         $scope.$apply(function() {
             $scope.colleges = results;
@@ -15,19 +17,21 @@ angular.module('yaleImsApp')
       	})
   	});
 
-  ParseService.getGames(undefined, undefined, false, function(results) {
+    GamesService.getGamesAttended(netid, undefined, undefined, function(results) {
         $scope.$apply(function() {
+                    // console.log(results);
           for(var game in results)
           {
-              if(results[game].team1.get('URL') == $rootScope.student.collegeurl 
-                  || results[game].team2.get('URL') == $rootScope.student.collegeurl) {
-                  results[game].showrsvp = true;
-              }
-              else {
-                  results[game].showrsvp = false;
-              }
+            if(results[game].team1.get('URL') == $rootScope.student.collegeurl 
+              || results[game].team2.get('URL') == $rootScope.student.collegeurl) {
+                results[game].showrsvp = true;
+            }
+            else {
+              results[game].showrsvp = false;
+            }
           }
           $scope.upcomingGames = results;
+          console.log(results);
         })
     });
 }]);
