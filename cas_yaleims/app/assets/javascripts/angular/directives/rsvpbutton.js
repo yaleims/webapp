27c5d3cf7960c49017e2c7cafe95c3c78b4f56ea
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Mac-Pro-2.local>
2014-04-20 02:35:01 -0400

    RSVP buttons showing by changing rsvp directive

--- cas_yaleims/app/assets/javascripts/angular/directives/rsvpbutton.js
+++ cas_yaleims/app/assets/javascripts/angular/directives/rsvpbutton.js
@@ -3,22 +3,22 @@
 angular.module('yaleImsApp')
   .directive('rsvpButton', ['GamesService', function (GamesService) {
     return {
-        template: '<button ng-show="!attending" ng-click="attend()" class="btn btn-info">RSVP</button><button ng-show="attending" ng-click="unattend()" class="btn btn-success">Attending</button>',
+        template: '<button ng-show="!going" ng-click="attend()" class="btn btn-info">RSVP</button><button ng-show="going" ng-click="unattend()" class="btn btn-success">Attending</button>',
         restrict: 'E',
         scope: {
             netid: '@',
             gameid: '@',
-            attending: '@'        
+            going: '@'        
         },
         link: function(scope) {
 
             scope.attend = function() {
-                scope.attending = true;
+                scope.going = true;
                 GamesService.attendGame(scope.netid, scope.gameid);
             };
 
             scope.unattend = function() {
-                scope.attending = false;
+                scope.going = false;
                 GamesService.unattendGame(scope.netid, scope.gameid);
             };
         }
