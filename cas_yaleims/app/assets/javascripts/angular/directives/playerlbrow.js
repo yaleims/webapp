848061abc9896b9d391f2a3bf9b1cbe6356e6cbb
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Mac-Pro-2.local>
2014-04-21 16:46:08 -0400

    Player links on home

--- cas_yaleims/app/assets/javascripts/angular/directives/playerlbrow.js
+++ cas_yaleims/app/assets/javascripts/angular/directives/playerlbrow.js
@@ -3,11 +3,14 @@
 angular.module('yaleImsApp')
   .directive('playerlbRow', function () {
     return {
-      template: '<td>{{rank}}</td><td>{{player}}</td><td>{{points}}</td>',
+      template: '<td>{{rank}}</td><td><shield college="{{college}}" collegeurl="{{collegeurl}}" size="smallShield"></shield><a ng-href="/profile/{{netid}}">{{player}}</td><td>{{points}}</td>',
       restrict: 'A',
       scope: {
           rank: '@',
           player: '@',
+          netid: '@',
+          college: '@',
+          collegeurl: '@',
           points: '@'
       }
     };
