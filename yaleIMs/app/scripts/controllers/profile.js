26d14184d53dab3530ff273ae21377b4e8d1de1a
nggonzalez <NicksFolder@Nicholas-Gonzalezs-Computer-2.local>
2014-04-22 13:02:55 -0400

    Remove yaleIMs folder -- old app with no ruby backend or CAS

--- yaleIMs/app/scripts/controllers/profile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('yaleImsApp')
-  .controller('ProfileCtrl', function ($scope) {
-    $scope.playerTeams = [{sport: "Coed Badminton", sporturl: "coed-badminton"}, {sport: "Coed Swimming", sporturl: "coed-swimming"}];
-    $scope.playerName = "Nicholas Gonzalez";
-        $scope.playerCollege = "Morse";
-        $scope.playerYear = 2015;
-  });
